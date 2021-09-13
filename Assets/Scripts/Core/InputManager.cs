13df419e3fba1f01f674c286b0e65f102681b992
Brandon Lacy <blacy15512gmail.com>
2021-09-13 14:18:39 -0500

    Added animator and camera follow

---
+++ Assets/Scripts/Core/InputManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class InputManager : MonoBehaviour
+    {
+        public static InputManager Instance;
+
+        public KeyCode InventoryPanel = KeyCode.I;
+        public KeyCode CharacterPanel = KeyCode.C;
+        public KeyCode Escape = KeyCode.Escape;
+
+        public float Horizontal { get; private set; }
+        public float Vertical { get; private set; }
+        public float MouseX { get; private set; }
+        public float MouseY { get; private set; }
+        public bool Action { get; private set; }
+        public Vector3 lookDir;
+
+        private Vector3 lookPos;
+
+        public void Awake()
+        {
+            Instance = this;
+        }
+        // Update is called once per frame
+        private void Update()
+        {
+            Horizontal = Input.GetAxis("Horizontal");
+            Vertical = Input.GetAxis("Vertical");
+            MouseX = Input.GetAxis("Mouse X");
+            MouseY = Input.GetAxis("Mouse Y");
+            Action = Input.GetMouseButtonUp(0);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (Vector3.Distance(transform.position, hit.point) > 1.5)
+                {
+                    lookPos = hit.point;
+                }
+            }
+            lookDir = lookPos - transform.position;
+            lookDir.y = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Player._Camera
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform target;
+        [SerializeField] private Texture2D cursor;
+        public float smoothSpeed = 0.125f;
+        public Vector3 offset;
+        public Vector3 lookPos;
+
+        private void LateUpdate()
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+
+        }
+    }
+}
